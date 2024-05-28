f8041cf0303152a62d2f59ea3f090104d88e9354
Juan Garcia <juangarcia123457854@gmail.com>
2024-05-27 23:08:09 -0500

    pruebas

--- lib/src/environment/environment.dart
+++ lib/src/environment/environment.dart
@@ -1,10 +1,8 @@
 class Enviroment {
 
   static const String API_URL = "https://apirest-web-version.onrender.com/";
-  static const String API_URL_OLD  = "https://apirest-web-version.onrender.com";
+  static const String API_URL_OLD  = "apirest-web-version.onrender.com";
 
-  // static const String API_URL  = "http://192.168.1.3:3000/";
-  // static const String API_URL_OLD  = "192.168.1.3:3000";
 
   static const String API_KEY_MAPS  = "AIzaSyDem4aCxEGa2fYzDujN3vkdqSXtuif-Us0";
 }
