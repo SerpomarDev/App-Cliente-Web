942e58da4cd5a59b1148940d7552e20bf5ffea30
Juan Garcia <juangarcia123457854@gmail.com>
2024-05-27 19:28:23 -0500

    puerto

--- lib/src/environment/environment.dart
+++ lib/src/environment/environment.dart
@@ -1,7 +1,7 @@
 class Enviroment {
 
-  static const String API_URL = "https://apirest-web-version.onrender.com/";
-  static const String API_URL_OLD  = "apirest-web-version.onrender.com";
+  static const String API_URL = "https://apirest-web-version.onrender.com:10000/";
+  static const String API_URL_OLD  = "https://apirest-web-version.onrender.com:10000";
 
   // static const String API_URL  = "http://192.168.1.3:3000/";
   // static const String API_URL_OLD  = "192.168.1.3:3000";
