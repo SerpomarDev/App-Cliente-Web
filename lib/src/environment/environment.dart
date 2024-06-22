f08761884ca9de71cf0d805f4cc7894d4029530c
Juan Garcia <159070696+SerpomarDev@users.noreply.github.com>
2024-06-22 15:58:16 -0500

    Update environment.dart

--- lib/src/environment/environment.dart
+++ lib/src/environment/environment.dart
@@ -1,7 +1,7 @@
 class Enviroment {
 
-  static const String API_URL = "https://apirest-web-version.onrender.com/";
-  static const String API_URL_OLD  = "apirest-web-version.onrender.com";
+  static const String API_URL = "https://190.109.19.165/";
+  static const String API_URL_OLD  = "190.109.19.165";
 
 
   static const String API_KEY_MAPS  = "AIzaSyDem4aCxEGa2fYzDujN3vkdqSXtuif-Us0";
